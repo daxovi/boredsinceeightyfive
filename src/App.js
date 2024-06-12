ed5cfafa03ab6adbd603c5294c624b4f3b0bc708
Dalibor Janecek <dalibor.janecek@gmail.com>
2024-06-12 16:44:23 +0000

    splash component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Splash from "./components/Splash";
+
+function App() {
+  return (
+    <div className="App">
+      <Splash />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Splash.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import img_b from "../img/b.svg";
+import img_s from "../img/s.svg";
+import img_8 from "../img/8.svg";
+import img_5 from "../img/5.svg";
+import "./Splash.css";
+
+const Splash = () => {
+  return (
+    <div>
+        <div className="splash">
+<img src={img_b} alt="" srcset="" />
+<img src={img_s} alt="" srcset="" />
+<img src={img_8} alt="" srcset="" />
+<img src={img_5} alt="" srcset="" />
+      </div>
+    </div>
+  )
+}
+
+export default Splash
